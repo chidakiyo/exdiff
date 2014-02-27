03c049d5a0e494a36c91e791870e14271ede75e2
kiyo <kiyokatsu.chida@weserve.co.jp>
2014-02-27 14:05:14 +0900

    ファイルをアップロードする仕組みを作成

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,4 +9,27 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def upload = Action(parse.multipartFormData) { request =>
+
+    for {
+      src <- request.body.file("srcFile")
+      dst <- request.body.file("dstFile")
+    } {
+      println(src.filename)
+      println(dst.filename)
+    }
+    Ok("File uploaded")
+
+    //    request.body.file("srcFile").map { srcfile =>
+    //      import java.io.File
+    //      val filename = srcfile.filename
+    //      val contentType = srcfile.contentType
+    //      srcfile.ref.moveTo(new File("/tmp/picture"))
+    //      Ok("File uploaded")
+    //    }.getOrElse {
+    //      Redirect(routes.Application.index).flashing(
+    //        "error" -> "Missing file")
+    //    }
+  }
+
 }
